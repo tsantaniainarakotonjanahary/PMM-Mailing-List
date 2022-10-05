94b36e8a9caaf086e13ab2e9b7f065697143ed18
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-10-05 17:10:13 +0300

    go

--- pmmail/src/renderer/App.tsx
+++ pmmail/src/renderer/App.tsx
@@ -1,56 +1,13 @@
-import {
-  MemoryRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import Accueil from './Accueil';
 import Login from './Login';
 import Group from './Group';
 import Mail from './Mail';
 import DocInterne from './DocInterne';
 import DocExterne from './DocExterne';
-import CreateGroup from './CreateGroup';
-import DeleteGroup from './DeleteGroup';
-import CreateMail from './CreateMail';
-import DeleteMail from './DeleteMail';
+
 import SendMail from './SendMail';
-
-/*
-const Hello = () => {
-  const navigate = useNavigate();
-
-  const [textArea, setTextArea] = useState('');
-
-  return (
-    <div>
-      <h1>Hello world</h1>
-      <label htmlFor="lab">
-        First name:
-        <textarea
-          value={textArea}
-          onChange={(event) => setTextArea(event.target.value)}
-        />
-      </label>
-
-      <p>{textArea}</p>
-      <button
-        type="button"
-        onClick={() => {
-          navigate('/dashboard', { replace: true });
-        }}
-      >
-        click
-      </button>
-    </div>
-  );
-};
-*/
-
 export default function App() {
   return (
     <div>
--- pmmail/src/renderer/Login.tsx
+++ pmmail/src/renderer/Login.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Sidebar from './Sidebar';
 
 function Login() {
   const navigate = useNavigate();
@@ -15,15 +18,52 @@ function Login() {
 
   return (
     <div>
-      Login
-      <button
-        type="button"
-        onClick={() => {
-          navigate('/', { replace: true });
-        }}
-      >
-        accueil
-      </button>
+      <Row className="mb-5">
+        <Col md={12}>
+          <Sidebar />
+        </Col>
+      </Row>
+      <Row className="mb-5">
+        <Col md={3}></Col>
+        <Col md={6}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Submit
+            </Button>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              S'inscrire
+            </Button>
+          </Form>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
     </div>
   );
 }
